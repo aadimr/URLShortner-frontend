9fa2d35933c205cb0523d958677124f4b995744c
aadimr <adityashaw9066@gmail.com>
2023-08-08 22:12:53 +0530

    landingPage and header responsivness is done

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,9 +3,9 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Header() {
   return (
-    <div className="shadow-lg sticky top-0 bg-white h-20 px-24">
+    <div className="shadow-lg sticky top-0 bg-white h-20 px-12">
        <div className='h-20 flex items-center justify-between'>
-        <img src={process.env.PUBLIC_URL + "/website-Logo.png"} alt='error' className='w-1/6 h-14 cursor-pointer'/>
+        <img src={process.env.PUBLIC_URL + "/website-Logo.png"} alt='error' className='w-1/4 h-14 cursor-pointer xl:w-1/6 lg:w-1/6'/>
         <AccountCircleIcon className='!text-[4rem] text-[#E8772E] cursor-pointer'/>
        </div>
     </div>
