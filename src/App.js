4e43fc89ece5a8fa011956e8e54e049498eae0a2
aadimr <adityashaw9066@gmail.com>
2023-08-13 22:32:22 +0530

    home page and footer page UI is done

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import LandingPage from './landingPage/LandingPage';
 import Header from './header/Header';
 import SingUP from './singUp/SingUp';
 import LogIn from './logIn/LogIn';
+import Home from './homePage/Home';
+import Footer from './footer/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      {/* <Header/> */}
+      <Header/>
       {/* <LandingPage/> */}
+      <Home/>
       <Routes>
         <Route path='/singUp' element={<SingUP/>}/>
         <Route path='/logIn' element={<LogIn/>}/>
+        {/* <Route path='/home'  element={<Home/>}/> */}
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ src/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='flex justify-center items-center bg-[#E8772E] text-white shadow-lg border h-[2rem]'>
+            <p>Copyright ©2023 All rights reserved | This website is made by Aditya Shaw</p>
+        </div>
+    )
+}
+
+export default Footer;
