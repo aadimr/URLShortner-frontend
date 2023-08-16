130bd3017b43619fe1a41c3245c35034d05d519b
aadimr <adityashaw9066@gmail.com>
2023-08-16 21:36:31 +0530

    home page and footer responsiveness is done

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className='flex justify-center items-center bg-[#E8772E] text-white shadow-lg border h-[2rem]'>
+        <div className='flex justify-center items-center bg-[#E8772E] text-white text-[.5rem] sm:text-[.7rem] md:text-[1rem] shadow-lg border h-[2rem]'>
             <p>Copyright ©2023 All rights reserved | This website is made by Aditya Shaw</p>
         </div>
     )
--- src/homePage/Home.jsx
+++ src/homePage/Home.jsx
@@ -3,13 +3,13 @@ import Input from "../components/input/Input";
 function Home() {
     return (
         <div className="flex flex-col items-center h-screen pt-[100px]">
-            <p className="font-bold text-[2rem] text-[#4f565a] mb-[40px] w-[40rem]">Create new</p>
-            <form>
-                <div className="w-5/6 sm:w-fit">
-                    <label className="text-[#6c6d71] font-semibold">Destination</label>
-                    <Input placeholder={"https://example.com/my-long-url"}
-                    //  name={"user_Name"} value={values.user_Name} onChange={handleChange} onBlur={handleBlur} 
-                     className="border border-[#d3d4d7] sm:w-[40rem] w-full mb-[18px] px-[11px] py-[10px] rounded-[3px] mt-[10px]" />
+            <p className="font-bold text-[2rem] text-[#4f565a] mb-[40px] md:w-[40rem] w-5/6">Create new</p>
+            <form className="flex flex-col items-center w-full md:w-auto">
+                <div className="w-5/6 flex flex-col justify-center md:w-fit">
+                    <label className="text-[#6c6d71] w-full w-5/6 mb-[.8rem] font-semibold">Destination</label>
+                    <Input placeholder={"Enter your name"} 
+                    // name={"user_Name"} value={values.user_Name} onChange={handleChange} onBlur={handleBlur} 
+                    className="border border-[#d3d4d7] md:w-[40rem] w-full mb-[18px] px-[11px] py-[10px] rounded-[3px]" />
                     {/* {errors.user_Name && touched.user_Name ? <p>*{errors.user_Name}</p> : null} */}
                 </div>
             </form>
