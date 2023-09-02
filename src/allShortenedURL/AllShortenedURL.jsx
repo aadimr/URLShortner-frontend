0f4e18139d08c1c2844488ce3c711f71d680b4b7
aadimr <adityashaw9066@gmail.com>
2023-09-02 22:39:27 +0530

    done

--- src/allShortenedURL/AllShortenedURL.jsx
+++ src/allShortenedURL/AllShortenedURL.jsx
@@ -13,7 +13,7 @@ function AllShortenedURL() {
 
     let [copied, setCopide] = useState(false)
 
-    let { allURLs } = useSelector(state => state.URL)
+    let { allURLs,loading } = useSelector(state => state.URL)
 
     const userDetail = JSON.parse(localStorage.getItem('loggedInUserDetail'));
 
@@ -28,45 +28,49 @@ function AllShortenedURL() {
         getAllShortenedUrls();
     }, [dispatch]);
 
+    if (loading) {
+        return <h2 className='text-[#6c6d71] text-[1.5rem] font-semibold flex justify-center items-center h-screen'>Loading...</h2>
+    }
+
     return (
         <>
-        <Header/>
-        <div className="flex flex-col items-center w-full min-h-screen pt-[6rem]">
-            {allShortedURLS.length !== 0 && recentShortedUrl ?
-                <>
-                    <div className="w-full flex flex-col justify-center items-center mt-[60px]">
-                        <p className="text-[#6c6d71] w-5/6 mb-[.8rem] font-semibold">Your last shortened URL</p>
-                        <div className="drop-shadow-lg bg-white min-h-20 w-5/6 mb-[18px] px-[11px] py-[10px] rounded-[3px] flex flex-col sm:flex-row items-center justify-between">
-                            <div className='flex flex-col gap-[1rem]'>
-                                <span className='text-black w-full break-all'>shortUrl: <Link to={recentShortedUrl.shortUrl} target="_blank" className='text-[#2a5bd7] hover:underline'>{recentShortedUrl.shortUrl}</Link></span>
-                                <span className='text-black w-full break-all'>longUrl: <Link to={recentShortedUrl.longUrl} target="_blank" className='text-[#2a5bd7] hover:underline'>{recentShortedUrl.longUrl}</Link></span>
-                            </div>
-                            <CopyToClipboard text={recentShortedUrl.shortUrl}
-                                onCopy={() => setCopide(true)}
-                            >
-                                <ContentCopyIcon className="cursor-pointer" />
-                            </CopyToClipboard>
-                        </div>
-                    </div>
-                    <div className="w-full flex flex-col justify-center items-center mt-[60px] mb-[50px]" >
-                        <p className="text-[#6c6d71] w-5/6 mb-[.8rem] font-semibold">Get your all shortened URLs</p>
-                        {allShortedURLS.map((ele, index) => (
-                            <div className="drop-shadow-lg bg-white min-h-20 w-5/6 mb-[18px] px-[11px] py-[10px] rounded-[3px] flex flex-col sm:flex-row items-center justify-between" key={index}>
+            <Header />
+            <div className="flex flex-col items-center w-full min-h-screen pt-[6rem]">
+                {allShortedURLS.length !== 0 && recentShortedUrl ?
+                    <>
+                        <div className="w-full flex flex-col justify-center items-center mt-[60px]">
+                            <p className="text-[#6c6d71] w-5/6 mb-[.8rem] font-semibold">Your last shortened URL</p>
+                            <div className="drop-shadow-lg bg-white min-h-20 w-5/6 mb-[18px] px-[11px] py-[10px] rounded-[3px] flex flex-col sm:flex-row items-center justify-between">
                                 <div className='flex flex-col gap-[1rem]'>
-                                    <span className='text-black w-full break-all'>shortUrl: <Link to={ele.shortUrl} target="_blank" className='text-[#2a5bd7] hover:underline'>{ele.shortUrl}</Link></span>
-                                    <span className='text-black w-full break-all'>longUrl: <Link to={ele.longUrl} target="_blank" className='text-[#2a5bd7] hover:underline'>{ele.longUrl}</Link></span>
+                                    <span className='text-black w-full break-all'>shortUrl: <Link to={recentShortedUrl.shortUrl} target="_blank" className='text-[#2a5bd7] hover:underline'>{recentShortedUrl.shortUrl}</Link></span>
+                                    <span className='text-black w-full break-all'>longUrl: <Link to={recentShortedUrl.longUrl} target="_blank" className='text-[#2a5bd7] hover:underline'>{recentShortedUrl.longUrl}</Link></span>
                                 </div>
-                                <CopyToClipboard text={ele.shortUrl}
+                                <CopyToClipboard text={recentShortedUrl.shortUrl}
                                     onCopy={() => setCopide(true)}
                                 >
-                                    <ContentCopyIcon className="cursor-pointer"/>
+                                    <ContentCopyIcon className="cursor-pointer" />
                                 </CopyToClipboard>
                             </div>
-                        ))}
-                    </div>
-                </> : <p className='text-[#6c6d71] text-[1.5rem] font-semibold flex justify-center items-center mt-[15rem]'>Empty</p>}
-        </div>
-        <Footer/>
+                        </div>
+                        <div className="w-full flex flex-col justify-center items-center mt-[60px] mb-[50px]" >
+                            <p className="text-[#6c6d71] w-5/6 mb-[.8rem] font-semibold">Get your all shortened URLs</p>
+                            {allShortedURLS.map((ele, index) => (
+                                <div className="drop-shadow-lg bg-white min-h-20 w-5/6 mb-[18px] px-[11px] py-[10px] rounded-[3px] flex flex-col sm:flex-row items-center justify-between" key={index}>
+                                    <div className='flex flex-col gap-[1rem]'>
+                                        <span className='text-black w-full break-all'>shortUrl: <Link to={ele.shortUrl} target="_blank" className='text-[#2a5bd7] hover:underline'>{ele.shortUrl}</Link></span>
+                                        <span className='text-black w-full break-all'>longUrl: <Link to={ele.longUrl} target="_blank" className='text-[#2a5bd7] hover:underline'>{ele.longUrl}</Link></span>
+                                    </div>
+                                    <CopyToClipboard text={ele.shortUrl}
+                                        onCopy={() => setCopide(true)}
+                                    >
+                                        <ContentCopyIcon className="cursor-pointer" />
+                                    </CopyToClipboard>
+                                </div>
+                            ))}
+                        </div>
+                    </> : <p className='text-[#6c6d71] text-[1.5rem] font-semibold flex justify-center items-center mt-[15rem]'>Empty</p>}
+            </div>
+            <Footer />
         </>
     )
 }
